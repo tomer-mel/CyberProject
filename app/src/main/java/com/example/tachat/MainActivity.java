45435ffc30a0ee2e1f87ccd93bdc957f5386bc9e
tomer-mel <tomer.melamed03@gmail.com>
2021-03-10 19:01:35 +0200

    commit

--- app/src/main/java/com/example/tachat/MainActivity.java
+++ app/src/main/java/com/example/tachat/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
     {
         if (IsValid())
         {
-            //next page
+            //next activity
         }
         //try again
     }
