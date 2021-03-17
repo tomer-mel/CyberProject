abda5b89d34744761dfbd2af3bf60752e08a9ebc
tomer-mel <tomer.melamed03@gmail.com>
2021-03-17 20:03:43 +0200

    commit

--- app/src/main/java/com/example/tachat/Logged_in.java
+++ app/src/main/java/com/example/tachat/Logged_in.java
@@ -21,32 +21,43 @@ public class Logged_in extends AppCompatActivity {
         String welcome = "welcome " + message;
         textView.setText(welcome);
     }
-    public void EncryptingText(View view){ // Encrypting the text and creates a key
+
+
+    public void EncryptText(String text){ // choosing which way to Encrypt the text in
 
     }
-    public void HideTextInImage(View view){ // hides the text and key in the image(need to figure out which image to use and from where), also need to figure out
+
+
+    public void HideTextInImage(byte[] encryptedmessage){ // hides the text and key in the image(need to figure out which image to use and from where), also need to figure out how to hide the text in the image.
 
     }
-    //public void SendImage(View view){ // sends the pic message to the server
-    //    TextView textView = findViewById(R.id.welcome);
-    //    String message = textView.getText().toString();
-    //   message = EncodeText(message);
-    //    Image = HideTextInImage(message);
-    public byte[], byte[] EncryptingTextWay1(View view){ // Encrypting the text and creates a key
-        TextView textView = findViewById(R.id.textline);
-        String text = textView.getText().toString();
+
+
+    //public void SendImage(View view, byte[] key){ // sends the pic message to the server
+    //    TextView textView = findViewById(R.id.textline);
+    //    String text = textView.getText().toString();
+    //    message = EncryptText(String text);
+    //    Image = HideTextInImage(byte[] encryptedmessage);
+
+
+    // need to figure out how to return the key(it doesn't support multi return)
+    public byte[] EncryptingTextWay1(String text){ // Encrypting the text and creates a key
         byte[] bin = text.getBytes();
-        String message = "veryveryveryveryveryveryveryveryveryveryveryverylongmessage";
-        byte[] key = message.getBytes();
+        String keymessage = "veryveryveryveryveryveryveryveryveryveryveryverylongmessage";
+        byte[] key = keymessage.getBytes();
         byte[] encryptedmessage = new byte[bin.length];
         for (int i = 0; i < bin.length;i++){
             encryptedmessage[i] = (byte) (bin[i] ^ key[i]);
         }
-        return encryptedmessage, key;
+        return encryptedmessage;
     }
+
+
     public void EncryptingTextWay2(View view){ // Encrypting the text and creates a key
 
     }
+
+
     public void EncryptingTextWay3(View view){ // Encrypting the text and creates a key
 
     }
