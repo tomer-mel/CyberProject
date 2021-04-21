b6459ca1364c2baad81b7a6b7487116dbe9f066d
tomer-mel <tomer.melamed03@gmail.com>
2021-04-21 18:13:33 +0300

    commit

--- app/src/main/java/com/example/tachat/Logged_in.java
+++ app/src/main/java/com/example/tachat/Logged_in.java
@@ -15,7 +15,7 @@ import java.util.Random;
 public class Logged_in extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {    //finished
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_in);
         Intent intent = getIntent();
@@ -23,19 +23,18 @@ public class Logged_in extends AppCompatActivity {
         TextView textView = findViewById(R.id.welcome);
         String welcome = "welcome " + message;
         textView.setText(welcome);
+        //LoadMessages();
     }
 
 
-    public byte[] EncryptText(String text, String key){ // choosing which way to Encrypt the text in
+    public byte[] EncryptText(String text, String key){     //finished
         int Rannum = new Random().nextInt(3) + 1;
         if (Rannum == 1)
             return(EncryptingTextWay1(text, key));
         if (Rannum == 2)
             return(EncryptingTextWay2(text, key));
-        if (Rannum == 3)
-            return(EncryptingTextWay3(text, key));
         else
-            return EncryptingTextWay1(text, key);
+            return(EncryptingTextWay3(text, key));
     }
     public String MakeRandomKey(){
         byte[] rad = new byte[40];
@@ -45,22 +44,22 @@ public class Logged_in extends AppCompatActivity {
     }
 
 
-    public Image HideTextInImage(byte[] encryptedmessage, String keymessage){ // hides the text and key in the ire out how to hide the text in the image.
+    public Image HideTextInImage(byte[] encryptedmessage, String key){ // hides the text and key in the image in three ways, need to  figure out how to hide everything in the image.
 
     }
 
 
-    //public void SendImage(View view, byte[] key){ // sends the pic message to the server
+    public void SendImage(View view) { // sends the pic message to the server
         TextView textView = findViewById(R.id.textline);
         String text = textView.getText().toString();
         String key = MakeRandomKey();
         byte[] encryptedmessage = EncryptText(text, key);
         Image img = HideTextInImage(encryptedmessage, key);
+    }
 
 
 
-
-    public byte[] EncryptingTextWay1(String text, String key){ // Encrypting the text
+    public byte[] EncryptingTextWay1(String text, String key){
         byte[] bin = text.getBytes();
         byte[] keybytes = key.getBytes();
         byte[] encryptedmessage = new byte[bin.length];
@@ -84,7 +83,8 @@ public class Logged_in extends AppCompatActivity {
 
 
 
-
+    //public void LoadMessages() // load previous messages from the server and display them on the screen
+    //{}
 
 
     }
