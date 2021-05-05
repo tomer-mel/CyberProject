6e0b1883b8712c6b63b81bde88cffacc22a9b372
tomer-mel <tomer.melamed03@gmail.com>
2021-05-05 19:04:36 +0300

    commit

--- app/src/main/java/com/example/tachat/Logged_in.java
+++ app/src/main/java/com/example/tachat/Logged_in.java
@@ -1,41 +1,53 @@
 package com.example.tachat;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.media.Image;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.os.Bundle;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
 import java.util.Random;
+
+// EncryptingTextWay2, HideTextInImageWay1, HideTextInImageWay2
+// server that gets every pic with message and holds a string of them and sends them to the app whenever it opens
+// make a database for pictures with different topics and chose random topic from which to send pictures
 
 public class Logged_in extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {    //finished
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_in);
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.welcome);
-        String welcome = "welcome " + message;
+        String welcome = "Welcome " + message;
         textView.setText(welcome);
-        //LoadMessages();
+        LoadMessages();
     }
 
 
-    public byte[] EncryptText(String text, String key){     //finished
-        int Rannum = new Random().nextInt(3) + 1;
-        if (Rannum == 1)
-            return(EncryptingTextWay1(text, key));
-        if (Rannum == 2)
-            return(EncryptingTextWay2(text, key));
-        else
-            return(EncryptingTextWay3(text, key));
+
+    public void SendImage(View view) { // sends the pic message to the server
+        TextView textView = findViewById(R.id.textline);
+        String text = textView.getText().toString();
+        String key = MakeRandomKey();
+        //    byte[] encryptedmessage = EncryptText(text, key);
+        //Image img = HideTextInImage(encryptedmessage, key);
     }
+
+
+
     public String MakeRandomKey(){
         byte[] rad = new byte[40];
         Random key = new Random();
@@ -44,22 +56,16 @@ public class Logged_in extends AppCompatActivity {
     }
 
 
-    public Image HideTextInImage(byte[] encryptedmessage, String key){ // hides the text and key in the image in three ways, need to  figure out how to hide everything in the image.
-
-    }
-
-
-    public void SendImage(View view) { // sends the pic message to the server
-        TextView textView = findViewById(R.id.textline);
-        String text = textView.getText().toString();
-        String key = MakeRandomKey();
-        byte[] encryptedmessage = EncryptText(text, key);
-        Image img = HideTextInImage(encryptedmessage, key);
-    }
+    //public byte[] EncryptText(String text, String key){
+    //    int Rannum = new Random().nextInt(2) + 1;
+    //    if (Rannum == 1)
+     //       return(EncryptingTextWay1(text, key));
+    //    else
+    //        return(EncryptingTextWay2(text, key));
+   // }
 
 
-
-    public byte[] EncryptingTextWay1(String text, String key){
+    public byte[] EncryptingTextWay1(String text, String key){   //finished
         byte[] bin = text.getBytes();
         byte[] keybytes = key.getBytes();
         byte[] encryptedmessage = new byte[bin.length];
@@ -70,22 +76,50 @@ public class Logged_in extends AppCompatActivity {
     }
 
 
-    public byte[] EncryptingTextWay2(String text, String key){ // Encrypting the text
+    //public byte[] EncryptingTextWay2(String text, String key){ // Encrypting the text
+    //}
 
+    //public Image HideTextInImage(byte[] encryptedmessage, String key){ // hides the text and key in the image in one of two ways, need to figure out how to hide everything in the image.
+    //    int Rannum = new Random().nextInt(2) + 1;
+    //    if (Rannum == 1)
+    //       return(HideTextInImageWay1(encryptedmessage, key));
+    //    else
+    //        return(HideTextInImageWay2(encryptedmessage, key));
+    //}
+
+    //public byte[] HideTextInImageWay1(byte[] encryptedmessage, String key){ // hides the text in the Image
+    //}
+
+    //public byte[] HideTextInImageWay2(byte[] encryptedmessage, String key){ // hides the text in the Image in a different way
+    //}
+
+
+
+    public void LoadMessages(){
+        new Thread(new Runnable(){
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void run() {
+                String info = "";
+                try{
+                    String ip= "192.168.14.5";
+                    int port = 6789;
+                    Socket s = new Socket(ip, port);
+                    DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+                    dout.writeUTF("GET");
+                    BufferedReader din = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                    info = din.readLine();
+                    din.close();
+                    dout.close();
+                    s.close();
+                    TextView txt = findViewById(R.id.messages);
+                    txt.append(info);
+                }catch(Exception e){System.out.println(e);}
+            }
+        }).start();
     }
 
 
-    public byte[] EncryptingTextWay3(String text, String key){ // Encrypting the text
 
-    }
-
-
-
-
-
-    //public void LoadMessages() // load previous messages from the server and display them on the screen
-    //{}
-
-
-    }
+}
 
--- app/src/main/java/com/example/tachat/MainActivity.java
+++ app/src/main/java/com/example/tachat/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.tachat;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
@@ -19,27 +20,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    //public boolean IsValid() //need to do a python server who will do that
-    //{
-    //    EditText editText = (EditText) findViewById(R.id.emailfill);
-    //    String username = editText.getText().toString();
-    //    EditText editText2 = (EditText) findViewById(R.id.passwordfill);
-    //    String password = editText2.getText().toString();
-    //    if (username.contains("@")){
-    //        if (password.length() > 5){
-    //            if (((username.equals(getString(R.string.useremail1))) || (username.equals(getString(R.string.useremail2)))) || (username.equals(getString(R.string.useremail3))) || (username.equals(getString(R.string.useremail4))))
-    //            {
-    //                if (((password.equals(getString(R.string.userpassword1))) || (password.equals(getString(R.string.userpassword2)))) || (password.equals(getString(R.string.userpassword3))) || (password.equals(getString(R.string.userpassword4)))) {
-    //                    return true;
-    //               }
-    //           }
-    //       }
-    //  }
-    //    return false;
-    //}
+    public boolean IsValid() // make sure the email ana password are valid
+    {
+        EditText editText = (EditText) findViewById(R.id.emailfill);
+        String username = editText.getText().toString();
+        EditText editText2 = (EditText) findViewById(R.id.passwordfill);
+        String password = editText2.getText().toString();
+        if (username.contains("@")){
+            if (password.length() > 5){
+                if (((username.equals(getString(R.string.useremail1))) || (username.equals(getString(R.string.useremail2)))) || (username.equals(getString(R.string.useremail3))) || (username.equals(getString(R.string.useremail4))))
+                {
+                     if (((password.equals(getString(R.string.userpassword1))) || (password.equals(getString(R.string.userpassword2)))) || (password.equals(getString(R.string.userpassword3))) || (password.equals(getString(R.string.userpassword4)))) {
+                        return true;
+                   }
+               }
+           }
+      }
+        return false;
+    }
     public void homepage(View view)
     {
-        if (IsValid()) //sends that to the server to check
+        if (IsValid())
         {
             Intent intent = new Intent(this, Logged_in.class);
             EditText editText = (EditText) findViewById(R.id.emailfill);
